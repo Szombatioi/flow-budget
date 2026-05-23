7f0c1229e97205c51de9494f79f5551927556b77
Szombati Olivér <o.szombati@gmail.com>
2026-05-23 16:06:02 +0200

    Fixed design + functionality on Expenses page + added export page base

--- FlowBudget/FlowBudget/FlowBudget.Client/Layout/CustomColors.cs
+++ FlowBudget/FlowBudget/FlowBudget.Client/Layout/CustomColors.cs
@@ -2,5 +2,6 @@ namespace FlowBudget.Client.Layout;
 
 public static class CustomColors
 {
-    public static string GreyPaper = "#f3f4f5";
+    public static readonly string GreyPaper = "#f3f4f5";
+    public static readonly string LightPaper = "#fff";
 }
